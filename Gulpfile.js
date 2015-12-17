7e52c96e3aec1102fc7c7ca716df31d5fa25db3b
Kane Clover <kane@assure24.com>
2015-12-17 14:12:04 +0000

    Don't lazypipe as there is no need....

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,24 +2,25 @@
 
 var gulp         = require( 'gulp' );
 var browserSync  = require( 'browser-sync' ).create();
-var lazypipe     = require( 'lazypipe' );
 var sass         = require( 'gulp-sass' );
 
-var cssTasks = lazypipe()
-	.pipe( sass, {
-		includePaths: [
-			'bower_components/bourbon/app/assets/stylesheets/',
-			'bower_components/neat/app/assets/stylesheets/'
-		]
-	});
-
-gulp.task( 'build', function() {
+// CSS pre-processing
+gulp.task( 'sass', function() {
 	return gulp.src( 'assets/styles/*.scss' )
-		.pipe( cssTasks() )
+		.pipe( sass({
+			includePaths: [
+				'bower_components/bourbon/app/assets/stylesheets/',
+				'bower_components/neat/app/assets/stylesheets/'
+			]
+		}) )
 		.pipe( gulp.dest( 'dist' ) )
 		.pipe( browserSync.stream() );
 });
 
+// Main build task
+gulp.task( 'build', [ 'sass' ] );
+
+// Watch files and run BrowserSync
 gulp.task( 'watch', ['build'], function() {
 	browserSync.init({
 		server: '.'
