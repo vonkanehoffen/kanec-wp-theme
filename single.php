6ed9979529d778da63484a513bd35264731c0942
Kane Clover <kane@assure24.com>
2016-01-01 11:33:02 +0000

    Post navigation

--- single.php
+++ single.php
@@ -17,7 +17,10 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			the_post_navigation([
+				'prev_text'          => '<i class="icon icon-arrow-left"></i> %title',
+				'next_text'          => '%title <i class="icon icon-arrow-right"></i>',
+				]);
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
